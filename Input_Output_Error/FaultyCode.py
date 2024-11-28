25c79cc36fb0089cbac0daa1f9e5aefefaea301e
MarioRaid99 <33650221+MarioRaid99@users.noreply.github.com>
2024-11-28 17:25:42 +0200

    Erinevad errorid mis võivad tulla

---
+++ Input_Output_Error/FaultyCode.py
@@ -0,0 +1,9 @@
+# print("Hello World) # Jutumärgid lõpetamata
+
+# print("Hello World") # Taande viga
+
+#if __name__ == '__main__':
+#print ("Hello World") # Taande viga, oodatud taane puudub
+
+#print (undeclared_variable + 18) #defineering puudub
+
